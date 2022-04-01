15291ad782e83f51a9ec5cc0a329f368010ae569
vitor.duggen <vitor.duggen@unimestre.com>
2022-04-01 09:02:02 -0300

    Foi alterado nome do arquivo pois foi tirado o breadcrumb e deixado apenas o titulo

---
+++ frontend/src/components/base/BaseHeader.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="breadcrumb">
+    <span class="breadcrumb__title">{{ title }}</span>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({})
+export default class BaseHeader extends Vue {
+  get title() {
+    return this.$route.meta?.name;
+  }
+}
+</script>
+
+<style lang="scss">
+@import "~vuetify/src/styles/main.sass";
+
+.breadcrumb {
+  &__title {
+    @extend .text-h4;
+    @extend .font-weight-bold;
+  }
+}
+</style>
