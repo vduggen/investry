0cbfaec0e76a29508855696b87f1a8e00f3b45a8
vitor.duggen <vitor.duggen@unimestre.com>
2022-04-01 09:03:52 -0300

    Criado componente de wrapper das páginas

---
+++ frontend/src/components/base/BaseWrapperPage.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-card class="card" elevation="0">
+    <slot />
+  </v-card>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class BaseWrapperPage extends Vue {}
+</script>
+
+<style lang="scss">
+@import "~vuetify/src/styles/main.sass";
+
+.card {
+  @extend .rounded-lg;
+  @extend .iy__h-100;
+  @extend .pa-6;
+}
+</style>
