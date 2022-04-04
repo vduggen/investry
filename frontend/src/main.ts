aabb719b6d29bc957c17d216e2a52ce2ace27f18
vitor.duggen <vitor.duggen@unimestre.com>
2022-04-04 13:19:21 -0300

    Adicionado o v-money

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,10 +1,13 @@
 import Vue from 'vue';
+import money from 'v-money';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false;
+
+Vue.use(money);
 
 new Vue({
   router,
