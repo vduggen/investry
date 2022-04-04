df22634508b5048085263184e8ad96844934dc36
vitor.duggen <vitor.duggen@unimestre.com>
2022-04-04 17:42:19 -0300

    Criado componentede tabela

---
+++ frontend/src/components/base/BaseTable.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-data-table
+    :headers="headers"
+    :items="items"
+    v-bind="$attrs"
+    v-on="$listeners"
+  >
+    <template
+      v-for="custom in customs"
+      #[`item.${custom}`]="{ item }"
+    >
+      <slot :name="custom" :item="item" />
+    </template>
+
+    <template #[`no-data`]>
+      Nenhum dado disponível
+    </template>
+  </v-data-table>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import ITableHeader from '../../typings/ITableHeader';
+
+@Component
+export default class BaseTable extends Vue {
+  @Prop({ default: [] }) headers!: ITableHeader[];
+
+  @Prop({ default: [] }) items!: [];
+
+  @Prop({ default: [] }) customs!: string[];
+}
+</script>
