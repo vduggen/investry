96d81bd3a639b794f58069576c768c2713ffa356
vitor.duggen <vitor.duggen@unimestre.com>
2022-04-04 17:44:35 -0300

    Adicionado icone de seta para voltar para tela anterior e adicionado um slot do lado direito e feito lógica de receber titulo

--- frontend/src/components/base/BaseHeader.vue
+++ frontend/src/components/base/BaseHeader.vue
@@ -1,16 +1,61 @@
 <template>
-  <div class="breadcrumb">
-    <span class="text-h4 font-weight-bold">{{ title }}</span>
-  </div>
+  <v-row class="header">
+    <v-col class="header__item header__item--left">
+      <BaseButton
+        v-if="viewRedirect"
+        @click="$router.go(-1)"
+        class="mr-3"
+        icon
+      >
+        <v-icon>mdi-arrow-left</v-icon>
+      </BaseButton>
+
+      <span class="text-h4 font-weight-bold">{{ titleComputed }}</span>
+    </v-col>
+
+    <v-col class="header__item header__item--right">
+      <slot name="right" />
+    </v-col>
+  </v-row>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import BaseButton from './BaseButton.vue';
 
-@Component({})
-export default class BaseHeader extends Vue {
-  get title(): string | undefined {
+const BaseHeaderProps = Vue.extend({
+  components: {
+    BaseButton,
+  },
+});
+
+@Component
+export default class BaseHeader extends BaseHeaderProps {
+  @Prop({ default: '' }) title!: string;
+
+  @Prop({ default: true }) viewRedirect!: boolean;
+
+  get titleComputed(): string | undefined {
+    if (this.title) {
+      return this.title;
+    }
+
     return this.$route.meta?.name;
   }
 }
 </script>
+
+<style lang="scss">
+.header__item {
+  display: flex;
+  align-items: center;
+
+  &--left {
+    justify-content: flex-start;
+  }
+
+  &--right {
+    justify-content: flex-end;
+  }
+}
+</style>
