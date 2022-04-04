519cf1778ddd4f425573389a8b28e3a015272f4b
vitor.duggen <vitor.duggen@unimestre.com>
2022-04-04 17:40:24 -0300

    Criado mock de items para listagem de categorias especificas

---
+++ frontend/src/mocks/items.ts
@@ -0,0 +1,62 @@
+import IItem from '../typings/IItem';
+
+const items: IItem[] = [
+  {
+    category: 1,
+    name: 'Salário',
+    value: 45000,
+    date: '2022-03-01',
+    description: '',
+  },
+  {
+    category: 1,
+    name: 'Salário',
+    value: 45000,
+    date: '2022-03-01',
+    description: '',
+  },
+  {
+    category: 1,
+    name: 'Salário',
+    value: 45000,
+    date: '2022-03-01',
+    description: '',
+  },
+  {
+    category: 1,
+    name: 'Salário',
+    value: 45000,
+    date: '2022-03-01',
+    description: '',
+  },
+  {
+    category: 1,
+    name: 'Salário',
+    value: 45000,
+    date: '2022-03-01',
+    description: '',
+  },
+  {
+    category: 1,
+    name: 'Salário',
+    value: 45000,
+    date: '2022-03-01',
+    description: '',
+  },
+  {
+    category: 1,
+    name: 'Salário',
+    value: 45000,
+    date: '2022-03-01',
+    description: '',
+  },
+  {
+    category: 1,
+    name: 'Salário',
+    value: 45000,
+    date: '2022-03-01',
+    description: '',
+  },
+];
+
+export default items;
