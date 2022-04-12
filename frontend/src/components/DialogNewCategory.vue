c2b6d21382c8b12aaddc238391c658749f0595c6
vitor.duggen <vitor.duggen@unimestre.com>
2022-04-12 15:36:30 -0300

    Feito a lógica de criar uma nova categoria, adicionado um loader, adicionado uma ação quando não tiver nenhum dado, feito integração API para trazer todas as categorias

---
+++ frontend/src/components/DialogNewCategory.vue
@@ -0,0 +1,162 @@
+<template>
+  <v-dialog v-model="show" width="500" class="dialog">
+    <template v-slot:activator="{ on, attrs }">
+      <BaseButton
+        large
+        primary
+        v-bind="attrs"
+        v-on="on"
+        prepend-icon="mdi-plus"
+      >
+        Adicionar
+      </BaseButton>
+    </template>
+
+    <v-card class="dialog__main">
+      <header class="dialog__header">
+        <BaseButton @click="closeDialog" color="grey" icon absolute top right>
+          <v-icon>mdi-close</v-icon>
+        </BaseButton>
+
+        <span class="dialog__title text-h6">Criar Categoria</span>
+      </header>
+
+      <v-card-text class="dialog__body">
+        <v-form
+          ref="formEditCategory"
+          v-model="valid"
+          lazy-validation
+        >
+          <BaseInput v-model="category.name" isRequired label="Nome" clearable />
+
+          <v-row>
+            <v-col class="pl-0">
+              <BaseSelect
+                isRequired
+                :items="colors"
+                label="Cor"
+                item-text="name"
+                item-value="id"
+                clearable
+                v-model="category.color_id"
+              />
+            </v-col>
+
+            <v-col class="pr-0">
+              <BaseInput v-model="category.icon" isRequired label="Nome do ícone" clearable />
+            </v-col>
+          </v-row>
+
+          <BaseTextArea v-model="category.description" hide-details label="Descrição" />
+        </v-form>
+      </v-card-text>
+
+      <footer class="d-flex justify-center">
+        <BaseButton
+          :loading="loading"
+          @click="createCategory"
+          large
+          primary
+        >
+          Salvar
+        </BaseButton>
+      </footer>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import BaseButton from './base/BaseButton.vue';
+import BaseInput from './base/BaseInput.vue';
+import BaseSelect from './base/BaseSelect.vue';
+import BaseDatePicker from './base/BaseDatePicker.vue';
+import BaseTextArea from './base/BaseTextArea.vue';
+import Color from '../services/Color';
+import { categoriesVuex } from '@/store';
+import Category, { IBodyCategory } from '@/services/Category';
+
+@Component({
+  components: {
+    BaseInput,
+    BaseButton,
+    BaseSelect,
+    BaseDatePicker,
+    BaseTextArea,
+  },
+})
+export default class DialogNewCategory extends Vue {
+  show = false;
+
+  colors = [];
+
+  valid = true;
+
+  loading = false;
+
+  HttpColor = new Color();
+
+  HttpCategory = new Category();
+
+  category = <IBodyCategory>{};
+
+  $refs!: {
+    formEditCategory: HTMLFormElement
+  };
+
+  async createCategory() {
+    this.loading = true;
+
+    const { data } = await this.HttpCategory.create(this.category);
+    
+    this.$toast.success(data.message);
+
+    const categories = this.$store.getters.getCategories;
+    const payload = [...categories, data.data];
+
+    this.$store.dispatch(categoriesVuex.GET_CATEGORIES, payload);
+
+    this.closeDialog();
+  }
+
+  closeDialog() {
+    this.loading = false;
+    this.show = false;
+    this.$refs.formEditCategory.reset();
+  }
+
+  mounted() {
+    this.getAllColors();
+  }
+
+  async getAllColors() {
+    const { data } = await this.HttpColor.all();
+
+    this.colors = data.data;
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.dialog {
+  border-radius: $border-radius;
+
+  &__main {
+    @include py(get-spacer(5));
+    @include px(get-spacer(8));
+  }
+
+  &__header {
+    @include d-flex(center, center, column);
+  }
+
+  &__title {
+    font-weight: 500;
+  }
+
+  &__body {
+    @include px(0 !important);
+    @include pt(get-spacer(5) !important);
+  }
+}
+</style>
--- frontend/src/views/CategoryList.vue
+++ frontend/src/views/CategoryList.vue
@@ -2,27 +2,38 @@
   <v-container class="home">
     <BaseHeader :viewRedirect="false">
       <template #right>
-        <DialogNewItem />
+        <DialogNewCategory />
       </template>
     </BaseHeader>
 
-    <v-row>
+    <v-row class="main__wrapper">
       <v-col class="iy__h-100">
         <BaseWrapperPage>
-          <v-row class="main__row">
-            <div v-for="(category, index) in categories" :key="`category-${index}`">
+          <div class="main__loader" v-if="loading">
+            <v-progress-circular
+              indeterminate
+              color="primary"
+              :size="70"
+            />
+          </div>
+
+          <v-row v-if="getCategories.length > 0" class="main__row">
+            <div v-for="(category, index) in getCategories" :key="`category-${index}`">
               <v-col>
-                <CardCategory
-                  :icon="category.icon"
-                  :title="category.name"
-                  :color="category.color"
-                  :description="category.description"
-                  :total="1500"
-                  @click="redirect(category)"
-                />
+                <CardCategory :category="category" />
               </v-col>
             </div>
           </v-row>
+
+          <div class="main__empty-data" v-else>
+            <img
+              src="@/assets/undraw_no_data_re_kwbl.svg"
+              alt="Sem dados"
+              width="170"
+            />
+
+            <span class="main__text text--secondary text-h6">Nenhuma categoria encontrada</span>
+          </div>
         </BaseWrapperPage>
       </v-col>
     </v-row>
@@ -31,31 +42,47 @@
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import { mapGetters } from 'vuex';
 import BaseHeader from '../components/base/BaseHeader.vue';
 import CardCategory from '../components/CardCategory.vue';
 import BaseWrapperPage from '../components/base/BaseWrapperPage.vue';
-import DialogNewItem from '../components/DialogNewItem.vue';
-import categories from '../mocks/categories';
-import ICategory from '../typings/ICategory';
+import DialogNewCategory from '../components/DialogNewCategory.vue';
+import Category from '../services/Category';
+import BaseButton from '../components/base/BaseButton.vue';
+import { categoriesVuex } from '../store';
+
+const CategoryListProps = Vue.extend({
+  computed: {
+    ...mapGetters(['getCategories']),
+  },
+});
 
 @Component({
   components: {
-    DialogNewItem,
+    DialogNewCategory,
     BaseWrapperPage,
     CardCategory,
     BaseHeader,
+    BaseButton,
   },
 })
-export default class CategoryList extends Vue {
-  categories: ICategory[] = categories;
+export default class CategoryList extends CategoryListProps {
+  loading = false;
 
-  redirect(routeInfo: ICategory) {
-    this.$router.push({
-      name: 'category-id',
-      params: {
-        id: `${routeInfo.id}`,
-      },
-    });
+  HttpCategory = new Category();
+
+  mounted() {
+    this.getAllCategories();
+  }
+
+  async getAllCategories() {
+    this.loading = true;
+
+    const { data } = await this.HttpCategory.all();
+
+    this.$store.dispatch(categoriesVuex.GET_CATEGORIES, data.data);
+
+    this.loading = false;
   }
 }
 </script>
@@ -63,8 +90,29 @@ export default class CategoryList extends Vue {
 <style lang="scss" scoped>
 .home {
   height: 100%;
-  .main__row {
-    @include d-flex;
+
+  .main {
+    &__loader {
+      height: 100%;
+      @include d-flex;
+    }
+
+    &__empty-data {
+      @include d-flex(center, center, column);
+      height: 100%;
+    }
+
+    &__text {
+      margin-top: get-spacer(5);
+    }
+
+    &__wrapper {
+      height: calc(100% - 68px);
+    }
+
+    &__row {
+      @include d-flex;
+    }
   }
 }
 </style>
