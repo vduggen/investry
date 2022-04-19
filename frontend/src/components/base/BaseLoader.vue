df16323187477d1928f595ea848d63e48b8e5efb
vitor.duggen <vitor.duggen@unimestre.com>
2022-04-19 10:09:27 -0300

    Criado componente de loader

---
+++ frontend/src/components/base/BaseLoader.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-progress-circular
+    indeterminate
+    color="primary"
+    :size="70"
+  />
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class BaseLoader extends Vue {}
+</script>
